536fd5dff8e1709476802067199b46b8a692317d
Markus Pöschl <Poeschl@users.noreply.github.com>
2026-03-11 23:38:50 +0100

    :tada: First steps to scheduled timetree exports

---
+++ timetree-exporter/execute-exporter.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+set -e
+
+EXPORT_FILE="/data/timetree_export.ical"
+
+if [ -z "$TIMETREE_EMAIL" ] || [ -z "$TIMETREE_PASSWORD" ]; then
+  echo "TIMETREE_EMAIL and TIMETREE_PASSWORD must both be set. Exiting."
+  exit 1
+fi
+
+EXTRA_ARGS=""
+if [ -n "$TIMETREE_CALENDER_CODE" ]; then
+  echo "Using calendar code: $TIMETREE_CALENDER_CODE"
+  EXTRA_ARGS="--calendar_code $TIMETREE_CALENDER_CODE"
+fi
+
+# Email and password are passed as environment variables.
+timetree-exporter \
+  --output "$EXPORT_FILE" \
+  $EXTRA_ARGS
+
+
